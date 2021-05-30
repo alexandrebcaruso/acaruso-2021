a69f3bb90d13a9854feec540fcb2a7c2992bd3b4
Alexandre Caruso <alequis@gmail.com>
2021-05-30 16:55:07 -0300

    root files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from './components/Logo';
+import "./App.scss";
+
+const App = () => {
+    return (
+        <>
+            <Logo />
+        </>
+    );
+};
+
+export default App;
