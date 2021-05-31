2f4919927017b235276bd03f464b454980ff1c42
Alexandre Caruso <alequis@gmail.com>
2021-05-31 15:11:36 -0300

    add link + style ajdusts

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./Logo.scss";
 
 const Logo = () => (
-    <svg className="logo">
-        <line x1="0" x2="50%" y1="100%" y2="0"></line>
-        <line x1="100%" x2="50%" y1="100%" y2="0"></line>
-    </svg>
+    <a className="logo" href="https://www.linkedin.com/in/alexandreborsattocaruso/" target="_blank">
+        <svg>
+            <line x1="0" x2="50%" y1="100%" y2="0"></line>
+            <line x1="100%" x2="50%" y1="100%" y2="0"></line>
+        </svg>
+    </a>
 )
 
 export default Logo;
--- src/components/triad/Triad.jsx
+++ src/components/triad/Triad.jsx
@@ -4,13 +4,13 @@ import "./Triad.scss";
 const Triad = () => (
     <svg className="triad" xmlns="http://www.w3.org/2000/svg">
         <circle cx="50%" cy="-40%" r="60%" className="plan" />
-        <text x="50%" y="-40%" text-anchor="middle">Plan</text>
+        <text x="50%" y="-40%" textAnchor="middle">Plan</text>
 
         <circle cx="80%" cy="60%" r="60%" className="design" />
-        <text x="80%" y="60%" text-anchor="middle">Design</text>
+        <text x="80%" y="60%" textAnchor="middle">Design</text>
         
         <circle cx="20%" cy="60%" r="60%" className="develop" />
-        <text x="20%" y="60%" text-anchor="middle">Develop</text>
+        <text x="20%" y="60%" textAnchor="middle">Develop</text>
     </svg>
 )
 
